cad85acb760e088fa634330c406cb4111c961159
Abdul Aziz Memon <abdul.aziz@invixible.com>
2023-12-10 02:55:38 +0500

    docker changes

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://18.193.113.247:5000",
+  baseURL: "http://localhost:5000",
 });
 
 API.interceptors.request.use((req) => {
